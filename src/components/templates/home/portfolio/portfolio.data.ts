e1589e0573c788ab35822f92e42d3bd0e22c78e4
Zero_i00 <mrshelkinpro228@gmail.com>
2024-12-07 22:23:50 +0500

    test image path in project

--- src/components/templates/home/portfolio/portfolio.data.ts
+++ src/components/templates/home/portfolio/portfolio.data.ts
@@ -3,21 +3,21 @@ import { IProject } from '@/types/project.types'
 
 export const PORTFOLIO_DATA: IProject[] = [
 	{
-		image: '/svg/projects/project1.svg',
+		image: '../../../../../public/svg/projects/project1.svg',
 		title: 'Sleep Tracker',
 		description: 'Мобильное приложение отслеживания и налаживания твоего сна',
-		link: 'https://brozex.ru/?ysclid=m4dx6knymr36576848'
+		link: 'https://www.figma.com/design/R4mSMTMSyi5vSatmlYZCXd/Sleep-Tracker?node-id=1-1478&t=eo9UhOzuaJCEHH05-1'
 	},
 	{
-		image: '/svg/projects/project2.svg',
+		image: '../../../../../public/svg/projects/project2.svg',
 		title: 'Learn Pass', 
 		description: 'Система управления школой программирования для отслеживания успеваемости учащихся, оплаты курсов и т.д',
 		link: 'https://learn.algoritmika.org/login'
 	},
 	{
-		image: '/svg/projects/project3.svg',
+		image: '../../../../../public/svg/projects/project3.svg',
 		title: 'Brozex App',
 		description: 'Мобильное приложение для розничной и оптовой торговли стройматериалами',
-		link: 'https://www.figma.com/design/R4mSMTMSyi5vSatmlYZCXd/Sleep-Tracker?node-id=1-1478&t=eo9UhOzuaJCEHH05-1'
+		link: 'https://brozex.ru/?ysclid=m4dx6knymr36576848'
 	}
 ]
